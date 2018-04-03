667d0456a5bacd883b3b755ffb49a9ebf3ef5d74
poorvashelke <32210401+poorvashelke@users.noreply.github.com>
2018-04-03 11:16:33 -0400

    Add files via upload

---
+++ Flask/data.py
@@ -0,0 +1,25 @@
+def Articles():
+    articles = [
+        {
+            'id': 1,
+            'title':'Article One',
+            'body':'body1',
+            'author':'Ash Shenoy',
+            'create_date':'08-28-2017'
+        },
+        {
+            'id': 2,
+            'title':'Article Two',
+            'body':'body2',
+            'author':'Ninad Joshi',
+            'create_date':'08-28-2017'
+        },
+        {
+            'id': 3,
+            'title':'Article Three',
+            'body':'body3',
+            'author':'Kinnary Gado',
+            'create_date':'08-28-2017'
+        }
+    ]
+    return articles
